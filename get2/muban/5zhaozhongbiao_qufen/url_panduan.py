6d7e985261722f983827926731c1578ff557b25b
abcd <1186514730@qq.com>
2022-12-26 14:19:07 +0800

    修改url_panduan规则

--- get2/muban/5zhaozhongbiao_qufen/url_panduan.py
+++ get2/muban/5zhaozhongbiao_qufen/url_panduan.py
@@ -1,8 +1,5 @@
-    def zhaozhong_biao(self, response, items):
-        for x in {a}:
-            if x in response.url:
-                items['channel_id'] = 15  # 中标15  招标16
-                break
-            else:
-                items['channel_id'] = 16  # 中标15  招标16
+    if re.findall(r{a}, str(response.url, 'utf-8'))[0] in {b}:  # 中标
+        items['channel_id'] = 15  # 中标15  招标16
+    else:
+        items['channel_id'] = 16  # 中标15  招标16
 
--- 接口替换.py
+++
@@ -1,6 +0,0 @@
-import os
-
-file_name_list = os.listdir(r'D:\pycharm_xiangmu\shishicesi\gerapy\projects\shishicesi\shishicesi\spiders')
-for file_name in file_name_list:
-    path = r'D:\pycharm_xiangmu\shishicesi\gerapy\projects\shishicesi\shishicesi\spiders\{}'.format(file_name)
-print(fileOrDirLst)
